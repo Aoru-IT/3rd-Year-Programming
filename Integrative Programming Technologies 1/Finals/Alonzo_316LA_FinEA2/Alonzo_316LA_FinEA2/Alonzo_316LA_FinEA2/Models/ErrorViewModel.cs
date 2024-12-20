799459ef72fcfedc856624136fbb14ffa98a3a40
Earl Lawrence Alonzo <144116937+Aoru-IT@users.noreply.github.com>
2024-12-20 08:14:01 +0800

    Update

--- Integrative Programming Technologies 1/Finals/Alonzo_316LA_FinEA2/Alonzo_316LA_FinEA2/Alonzo_316LA_FinEA2/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Alonzo_316LA_FinEA2.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Integrative Programming Technologies 1/Finals/Alonzo_316la_FinEA1/Alonzo_WebAppStudentGrades/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Alonzo_WebAppStudentGrades.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
