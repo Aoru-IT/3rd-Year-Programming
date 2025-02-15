361d244a691e93c9cfb03b90e84383e61b4e6e25
Earl Lawrence Alonzo <144116937+Aoru-IT@users.noreply.github.com>
2025-02-15 17:00:58 +0800

    Update

---
+++ 1st Semester/Integrative Programming Technologies 1/Finals/Alonzo_NewbookRecords/Alonzo_NewbookRecords/Alonzo_NewbookRecords/Alonzo_NewbookRecords/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alonzo_NewbookRecords.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
